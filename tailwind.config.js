995c8020ae89cab34d81013b6a21ba9cc16a424b
Paweł Pacana <pawel.pacana@gmail.com>
2024-11-26 17:06:30 +0100

    Colors as in the library

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,27 @@ module.exports = {
         "inter": ["Inter", "sans-serif"],
       },
       colors: {
-        brand: "#ED1C25"
+        red: {
+          logo: "#ED1C25",
+          100: "#BE161E",
+          300: "#8E1116",
+          500: "#5F0B0F",
+          700: "#47080B",
+          900: "#2F0607"
+        },
+        grey: {
+          100: "#F6F6F6",
+          200: "#D3D2D2",
+          300: "#BDBCBC",
+          400: "#A7A5A6",
+          500: "#7B7979",
+          600: "#4F4C4D",
+          logo: "#231F20",
+          800: "#161314",
+        },
+        accent: {
+          focus: "#0448CF"
+        }
       }
     },
   },
