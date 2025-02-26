c9f23701491bba72702b2a829c51a607b0d7fc8f
Paweł Pacana <pawel.pacana@gmail.com>
2025-02-26 13:48:07 +0100

    Smarter than a water meter

--- lib/world.rb
+++ lib/world.rb
@@ -1,2 +1,10 @@
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Rendering
+
+def speakers
+  speaker = Data.define(:name, :title, :photo)
+  [
+    speaker.new(name: "Adam Dymitruk", title: "Event Modelling from Beginner to Expert", photo: "speakers/adam_dymitruk.jpg"),
+    speaker.new(name: "You", title: "Your fabuluous talk", photo: nil)
+  ]
+end
