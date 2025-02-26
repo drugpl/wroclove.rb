416ca426be6d061167fb9544b3742b48155c1373
Paweł Pacana <pawel.pacana@gmail.com>
2025-02-26 12:48:33 +0100

    Doh, watch out for those ERBs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./layouts/**/*.html",
-    "./content/**/*.html",
-  ],
+  content: ["./layouts/**/*.{html,erb}", "./content/**/*.{html,erb}"],
   theme: {
     screens: {
       sm: "375px",
